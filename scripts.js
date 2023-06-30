ac57df3c586d1254abc85b5a6031f948f4caa1a8
BeJuanitox <jsebastian23@hotmail.com>
2023-06-30 15:50:55 -0500

    Add first functionality on activity one

---
+++ scripts.js
@@ -0,0 +1,355 @@
+var optActA1 = document.getElementById('optionA1');
+var optActA2 = document.getElementById('optionA2');
+var optActA3 = document.getElementById('optionA3');
+var optActA4 = document.getElementById('optionA4');
+var optActA5 = document.getElementById('optionA5');
+var optActA6 = document.getElementById('optionA6');
+var optActA7 = document.getElementById('optionA7');
+var optActA8 = document.getElementById('optionA8');
+
+function checkA1() {
+  optActA1.classList.add('correct');
+}
+
+function checkA2() {
+  optActA2.classList.add('wrong');
+}
+
+function checkA3() {
+  optActA3.classList.add('correct');
+}
+
+function checkA4() {
+  optActA4.classList.add('correct');
+}
+
+function checkA5() {
+  optActA5.classList.add('correct');
+}
+
+function checkA6() {
+  optActA6.classList.add('wrong');
+}
+
+function checkA7() {
+  optActA7.classList.add('wrong');
+}
+
+function checkA8() {
+  optActA8.classList.add('wrong');
+}
+
+// ------------------------ 2 -------------------------------
+
+var optActB1 = document.getElementById('optionB1');
+var optActB2 = document.getElementById('optionB2');
+var optActB3 = document.getElementById('optionB3');
+var optActB4 = document.getElementById('optionB4');
+var optActB5 = document.getElementById('optionB5');
+var optActB6 = document.getElementById('optionB6');
+var optActB7 = document.getElementById('optionB7');
+var optActB8 = document.getElementById('optionB8');
+
+function checkB1() {
+  optActB1.classList.add('correct');
+}
+
+function checkB2() {
+  optActB2.classList.add('correct');
+}
+
+function checkB3() {
+  optActB3.classList.add('correct');
+}
+
+function checkB4() {
+  optActB4.classList.add('wrong');
+}
+
+function checkB5() {
+  optActB5.classList.add('wrong');
+}
+
+function checkB6() {
+  optActB6.classList.add('wrong');
+}
+
+function checkB7() {
+  optActB7.classList.add('wrong');
+}
+
+function checkB8() {
+  optActB8.classList.add('correct');
+}
+
+// ---------------  3  -------------
+
+var optActC1 = document.getElementById('optionC1');
+var optActC2 = document.getElementById('optionC2');
+var optActC3 = document.getElementById('optionC3');
+var optActC4 = document.getElementById('optionC4');
+var optActC5 = document.getElementById('optionC5');
+var optActC6 = document.getElementById('optionC6');
+var optActC7 = document.getElementById('optionC7');
+var optActC8 = document.getElementById('optionC8');
+
+function checkC1() {
+  optActC1.classList.add('wrong');
+}
+
+function checkC2() {
+  optActC2.classList.add('wrong');
+}
+
+function checkC3() {
+  optActC3.classList.add('correct');
+}
+
+function checkC4() {
+  optActC4.classList.add('correct');
+}
+
+function checkC5() {
+  optActC5.classList.add('correct');
+}
+
+function checkC6() {
+  optActC6.classList.add('correct');
+}
+
+function checkC7() {
+  optActC7.classList.add('wrong');
+}
+
+function checkC8() {
+  optActC8.classList.add('correct');
+}
+
+// ------------  4  -----------
+
+var optActD1 = document.getElementById('optionD1');
+var optActD2 = document.getElementById('optionD2');
+var optActD3 = document.getElementById('optionD3');
+var optActD4 = document.getElementById('optionD4');
+var optActD5 = document.getElementById('optionD5');
+var optActD6 = document.getElementById('optionD6');
+var optActD7 = document.getElementById('optionD7');
+var optActD8 = document.getElementById('optionD8');
+
+function checkD1() {
+  optActD1.classList.add('wrong');
+}
+
+function checkD2() {
+  optActD2.classList.add('wrong');
+}
+
+function checkD3() {
+  optActD3.classList.add('correct');
+}
+
+function checkD4() {
+  optActD4.classList.add('correct');
+}
+
+function checkD5() {
+  optActD5.classList.add('wrong');
+}
+
+function checkD6() {
+  optActD6.classList.add('correct');
+}
+
+function checkD7() {
+  optActD7.classList.add('wrong');
+}
+
+function checkD8() {
+  optActD8.classList.add('correct');
+}
+
+// -------------  5  ---------------
+
+var optActE1 = document.getElementById('optionE1');
+var optActE2 = document.getElementById('optionE2');
+var optActE3 = document.getElementById('optionE3');
+var optActE4 = document.getElementById('optionE4');
+var optActE5 = document.getElementById('optionE5');
+var optActE6 = document.getElementById('optionE6');
+var optActE7 = document.getElementById('optionE7');
+var optActE8 = document.getElementById('optionE8');
+
+function checkE1() {
+  optActE1.classList.add('wrong');
+}
+
+function checkE2() {
+  optActE2.classList.add('correct');
+}
+
+function checkE3() {
+  optActE3.classList.add('correct');
+}
+
+function checkE4() {
+  optActE4.classList.add('wrong');
+}
+
+function checkE5() {
+  optActE5.classList.add('wrong');
+}
+
+function checkE6() {
+  optActE6.classList.add('correct');
+}
+
+function checkE7() {
+  optActE7.classList.add('correct');
+}
+
+function checkE8() {
+  optActE8.classList.add('wrong');
+}
+
+// -------- 6 --------
+
+var optActF1 = document.getElementById('optionF1');
+var optActF2 = document.getElementById('optionF2');
+var optActF3 = document.getElementById('optionF3');
+var optActF4 = document.getElementById('optionF4');
+var optActF5 = document.getElementById('optionF5');
+var optActF6 = document.getElementById('optionF6');
+var optActF7 = document.getElementById('optionF7');
+var optActF8 = document.getElementById('optionF8');
+
+function checkF1() {
+  optActF1.classList.add('wrong');
+}
+
+function checkF2() {
+  optActF2.classList.add('correct');
+}
+
+function checkF3() {
+  optActF3.classList.add('wrong');
+}
+
+function checkF4() {
+  optActF4.classList.add('correct');
+}
+
+function checkF5() {
+  optActF5.classList.add('wrong');
+}
+
+function checkF6() {
+  optActF6.classList.add('correct');
+}
+
+function checkF7() {
+  optActF7.classList.add('correct');
+}
+
+function checkF8() {
+  optActF8.classList.add('wrong');
+}
+
+// -------- 7 --------
+
+var optActG1 = document.getElementById('optionG1');
+var optActG2 = document.getElementById('optionG2');
+var optActG3 = document.getElementById('optionG3');
+var optActG4 = document.getElementById('optionG4');
+var optActG5 = document.getElementById('optionG5');
+var optActG6 = document.getElementById('optionG6');
+var optActG7 = document.getElementById('optionG7');
+var optActG8 = document.getElementById('optionG8');
+
+function checkG1() {
+  optActG1.classList.add('correct');
+}
+
+function checkG2() {
+  optActG2.classList.add('wrong');
+}
+
+function checkG3() {
+  optActG3.classList.add('correct');
+}
+
+function checkG4() {
+  optActG4.classList.add('wrong');
+}
+
+function checkG5() {
+  optActG5.classList.add('wrong');
+}
+
+function checkG6() {
+  optActG6.classList.add('correct');
+}
+
+function checkG7() {
+  optActG7.classList.add('correct');
+}
+
+function checkG8() {
+  optActG8.classList.add('wrong');
+}
+
+// -------- 8 --------
+
+var optActH1 = document.getElementById('optionH1');
+var optActH2 = document.getElementById('optionH2');
+var optActH3 = document.getElementById('optionH3');
+var optActH4 = document.getElementById('optionH4');
+var optActH5 = document.getElementById('optionH5');
+var optActH6 = document.getElementById('optionH6');
+var optActH7 = document.getElementById('optionH7');
+var optActH8 = document.getElementById('optionH8');
+
+function checkH1() {
+  optActH1.classList.add('wrong');
+}
+
+function checkH2() {
+  optActH2.classList.add('correct');
+}
+
+function checkH3() {
+  optActH3.classList.add('correct');
+}
+
+function checkH4() {
+  optActH4.classList.add('wrong');
+}
+
+function checkH5() {
+  optActH5.classList.add('correct');
+}
+
+function checkH6() {
+  optActH6.classList.add('wrong');
+}
+
+function checkH7() {
+  optActH7.classList.add('wrong');
+}
+
+function checkH8() {
+  optActH8.classList.add('correct');
+}
+
+
+
+/*
+2:
+  2 = 4
+  1 = 2
+  3 = 6
+  4 = 3
+  5 = 1
+  6 = 5
+  7 = 8
+  8 = 7
+*/
